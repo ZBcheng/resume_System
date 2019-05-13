39a739138193b8bae1ffb7d1b96cff3f08db54c2
Bee <Bee0_0@icloud.com>
2019-05-14 00:31:35 +0800

    增加数据库查询

--- src/User.java
+++ src/User.java
@@ -1,3 +1,8 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+
 public class User {
     private int id;
     private String name; // 姓名
@@ -16,129 +21,195 @@ public class User {
     private String image; // 图片
     private String homepage; // 个人主页
 
+    private String JDBC_DRIVER;
+    private String DB_URL;
 
-    public int getId() {
-        return this.id;
-    }
+    private String USER;
+    private String PASS;
 
-    public String getName() {
-        return this.name;
-    }
+    private Connection conn;
+    private Statement stmt;
 
-    public int getAge() {
-        return this.age;
-    }
 
-    public String getBirth() {
-        return this.birthday;
-    }
-
-    public String getGen() {
-        return this.gender;
-    }
-
-    public String getPhone() {
-        return this.phone;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getCity() {
-        return this.city;
-    }
-
-    public String getDesc() {
-        return this.desc;
-    }
-
-    public String getSchool() {
-        return this.school;
-    }
-
-    public String getMajor() {
-        return this.major;
-    }
-
-    public String getSkill() {
-        return this.skill;
-    }
-
-    public String getPrize() {
-        return this.prize;
-    }
-
-    public String getImage() {
-        return this.image;
-    }
-
-    public String getHomepage() {
-        return this.homepage;
-    }
-
-    public void setName(String name) {
+    User(int id, String name, String password, int age, String birthday, String gender, String phone, String email, String city, String desc, String school, String major, String skill, String prize, String image, String homepage) {
+        this.connect();
+        this.id = id;
         this.name = name;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    public void setAge(int age) {
         this.age = age;
-    }
-
-    public void setBirth(String birthday) {
         this.birthday = birthday;
-    }
-
-    public void setGender(String gender) {
         this.gender = gender;
-    }
-
-    public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
-    }
-
-    public void setCity(String city) {
         this.city = city;
-    }
-
-    public void setDesc(String desc) {
         this.desc = desc;
-    }
-
-    public void setSchool(String school) {
         this.school = school;
-    }
-
-    public void setMajor(String major) {
         this.major = major;
-    }
-
-    public void setSkill(String skill) {
         this.skill = skill;
-    }
-
-    public void setPrize(String prize) {
         this.prize = prize;
-    }
-
-    public void setImage(String image) {
         this.image = image;
-    }
-
-    public void setHomepage(String homepage) {
         this.homepage = homepage;
     }
 
+
+    public void connect() {
+        // JDBC 驱动名及数据库 URL
+        this.JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        this.DB_URL = "jdbc:mysql://localhost:3306/resume";
+
+        // 数据库的用户名与密码，需要根据自己的设置
+        this.USER = "root";
+        this.PASS = "Z1998bc0316";
+
+        this.conn = null;
+        this.stmt = null;
+        System.out.println("数据库连接成功");
+    }
+
+
+    // 查询
+    public void select(String username) {
+
+        try{
+            // 注册 JDBC 驱动
+            Class.forName("com.mysql.jdbc.Driver");
+
+            // 打开链接
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            System.out.print("done!");
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM User WHERE name = #{username} ";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // 展开结果集数据库
+            while(rs.next()){
+                // 通过字段检索
+                int id  = rs.getInt("id");
+                String name = rs.getString("name");
+                String password = rs.getString("password");
+                int age = rs.getInt("age");
+                String birthday = rs.getString("birthday");
+                String gender = rs.getString("gender");
+                String phone = rs.getString("phone");
+                String email = rs.getString("email");
+                String city = rs.getString("city");
+                String desc = rs.getString("des");
+                String school = rs.getString("school");
+                String major = rs.getString("major");
+                String skill = rs.getString("skill");
+                String prize = rs.getString("prize");
+                String image = rs.getString("image");
+                String homepage = rs.getString("homepage");
+
+                // 输出数据
+                System.out.print(id + name + password + age + birthday + gender + phone + email + city + desc + school + major + skill + prize + image + homepage);
+                System.out.print("\n");
+            }
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+
+    public void insert(User user) {
+
+        try{
+            // 注册 JDBC 驱动
+            Class.forName("com.mysql.jdbc.Driver");
+            int i = 0;
+            // 打开链接
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            System.out.print("done!");
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+//            stmt = conn.createStatement();
+            String sql;
+            sql = "insert into User values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement stmt;
+            try {
+                stmt = (PreparedStatement) conn.prepareStatement(sql);
+                stmt.setInt(1, this.id);
+                stmt.setString(2, this.name);
+                stmt.setString(3, this.password);
+                stmt.setInt(4, this.age);
+                stmt.setString(5, this.birthday);
+                stmt.setString(6, this.gender);
+                stmt.setString(7, this.phone);
+                stmt.setString(8, this.email);
+                stmt.setString(9, this.city);
+                stmt.setString(10, this.desc);
+                stmt.setString(11, this.school);
+                stmt.setString(12, this.major);
+                stmt.setString(13, this.skill);
+                stmt.setString(14, this.prize);
+                stmt.setString(15, this.image);
+                stmt.setString(16, this.homepage);
+                stmt.executeUpdate();
+                stmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.out.println("heh");
+            // 展开结果集数据库
+
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+                // 处理 Class.forName 错误
+                e.printStackTrace();
+            }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+
+    // 从前端获取数据后存入数据库
+    public void setInfo() {
+
+        this.insert(this);
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        User user = new User(1, "张毕成", "980110", 21, "980316", "male", "Bee0_0@icloud.com", "Bee0_0@icloud.com", "Xi'an", "hello", "Xi'an Shiyou University", "CS", "no", "no", "no", "zbcheng.github.io");
+        user.setInfo();
+//        user.select("张毕成");
     }
 
 }
